c24d8d192fdfa838023fc6520927853c5086b642
Pedro Manoel <dev.pedromanoel@pm.me>
2023-05-24 13:36:34 -0300

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBank::Application.config.secret_key_base = '23286c1a4fcc1d2963505fefa1e2ee1514a60eb247821fae6b73390945df8d4c40bed957ed28e871ba7d166e58d344f9c3baa307a3ac1f0dac3e47a17554eb41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsBank::Application.config.session_store :cookie_store, key: '_rails-bank_session'
