b648be9964eaa2a2468ef03e53f99d64ce998cf6
Pedro Manoel <dev.pedromanoel@pm.me>
2023-06-02 16:08:47 -0300

    Redirects to root_path after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_up_path_for(resource)
     root_path
-  end 
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 end
